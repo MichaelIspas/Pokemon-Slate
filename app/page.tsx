32df29a55f8a6f6db89dc3e4a295bd0f02a7138a
Michael <michael.ispas@gmail.com>
2026-02-05 10:19:32 -0800

    wip: add account mechanics for front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,86 +1,21 @@
-import PokemonCard from '@/components/pokemoncard'
-import RegionFilter from '@/components/regionfilter'
+// app/page.tsx
+import Link from 'next/link';
 
-export default async function Home({ searchParams }: { searchParams: { region?: string } }) {  
-  const params = await searchParams;
-  
-  // Decide range based on region selection
-  // Default = National (1-1025 and counting)
-  let start = 1
-  let end = 1025
+export default function Home() {
+    return (
+        <main className="min-h-screen bg-zinc-50 dark:bg-black flex flex-col items-center justify-center px-6 text-center">
+            <div className="max-w-4xl mx-auto space-y-6">
+                <h1 className="text-5xl md:text-7xl font-extrabold text-black dark:text-white tracking-tight">
+                    Pokémon Slate
+            </h1>
 
-  if (params.region === 'kanto') {
-    end = 151;
-  } else if (params.region === 'johto') {
-    start = 152;
-    end = 251;
-  } else if (params.region === 'hoenn') {
-    start = 252;
-    end = 386;
-  } else if (params.region === 'sinnoh') {
-    start = 387;
-    end = 493;
-  } else if (params.region === 'unova') {
-    start = 494;
-    end = 649;
-  } else if (params.region === 'kalos') {
-    start = 650;
-    end = 721;
-  } else if (params.region === 'alola') {
-    start = 722;
-    end = 809;
-  } else if (params.region === 'galar') {
-    start = 810;
-    end = 905;
-  } else if (params.region === 'paldea') {
-    start = 906;
-    end = 1025;
-  }
-  
-  // Fetch Pokemon IDs from 1 to 1025
-  const pokemonIds = Array.from({ length: end - start + 1 }, (_, i) => start + i);
-  
-  // Create array of promises, each promise fetches one Pokemon
-  const pokemonPromises = pokemonIds.map(async (id) => {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
-    cache: 'force-cache'
-    });
-
-    if (!res.ok) {
-      throw new Error('Failed to fetch Pokémon #${id}')
-    }
-
-    return res.json();
-  });
-
-  // Wait for all fetches to complete
-  let pokemons = [];
-  try {
-    pokemons = await Promise.all(pokemonPromises);
-  } catch (error) {
-    console.error(error);
-  }
-
-  return (
-    <div className="min-h-screen bg-zinc-50 dark:bg-black flex flex-col items-center pt-16 px-4">
-      <div className="text-center w-full max-w-5xl">
-        <h1 className="text-5xl font-bold text-black dark:text-white md:text-6xl">
-          Pokémon Slate
-        </h1>
-
-        <div className="text-left w-full max-w-5xl">
-          <RegionFilter currentRegion={params.region} />
-
-          <div className="grid grid-cols-6 auto-rows-[140px] mt-4">
-            {pokemons.map((pokemon) => (
-              <PokemonCard 
-                key={pokemon.id} 
-                pokemon={pokemon} 
-            />
-          ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+            <p>Your Ultimate Pokedex Monitor.</p>
+            <p>Keep track of every single Pokémon you attain throughout your journey as a Pokémon master.</p>
+            
+            <p>
+                <Link href="/pokedex">Check out your Pokédex</Link>
+            </p>
+            </div>
+        </main>
+    );
 }
---
+++ app/pokedex/page.tsx
@@ -0,0 +1,92 @@
+import PokemonCard from '@/components/pokemoncard'
+import RegionFilter from '@/components/regionfilter'
+import Link from 'next/link'
+
+export default async function Pokedex({ searchParams }: { searchParams: { region?: string } }) {  
+  const params = await searchParams;
+  
+  // Decide range based on region selection
+  // Default = National (1-1025 and counting)
+  let start = 1
+  let end = 1025
+
+  if (params.region === 'kanto') {
+    end = 151;
+  } else if (params.region === 'johto') {
+    start = 152;
+    end = 251;
+  } else if (params.region === 'hoenn') {
+    start = 252;
+    end = 386;
+  } else if (params.region === 'sinnoh') {
+    start = 387;
+    end = 493;
+  } else if (params.region === 'unova') {
+    start = 494;
+    end = 649;
+  } else if (params.region === 'kalos') {
+    start = 650;
+    end = 721;
+  } else if (params.region === 'alola') {
+    start = 722;
+    end = 809;
+  } else if (params.region === 'galar') {
+    start = 810;
+    end = 905;
+  } else if (params.region === 'paldea') {
+    start = 906;
+    end = 1025;
+  }
+  
+  // Fetch Pokemon IDs from 1 to 1025
+  const pokemonIds = Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  
+  // Create array of promises, each promise fetches one Pokemon
+  const pokemonPromises = pokemonIds.map(async (id) => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: 'force-cache'
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch Pokémon #${id}')
+    }
+
+    return res.json();
+  });
+
+  // Wait for all fetches to complete
+  let pokemons = [];
+  try {
+    pokemons = await Promise.all(pokemonPromises);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return (
+    <div className="min-h-screen bg-zinc-50 dark:bg-black flex flex-col items-center pt-16 px-4">
+      <div className="text-center w-full max-w-5xl">
+        <h1 className="text-5xl font-bold text-black dark:text-white md:text-6xl">
+          Pokémon Slate
+        </h1>
+        <h2>
+          <p>
+            <Link href="/">Back to Home</Link>
+          </p>
+        </h2>
+
+        <div className="text-left w-full max-w-5xl">
+          <RegionFilter currentRegion={params.region} />
+
+          <div className="grid grid-cols-6 auto-rows-[140px] mt-4">
+            {pokemons.map((pokemon) => (
+              <PokemonCard 
+                key={pokemon.id} 
+                pokemon={pokemon} 
+            />
+          ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
